bc93b9ac6bdfec4f1a912085516afcfd52a4073b
LC_022 <101245833+sblzdddd@users.noreply.github.com>
2026-03-29 23:00:43 +0800

    migrate to & initialize tauri project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from "vite";
 import path from "path";
+import { defineConfig } from "vite";
 
-export default defineConfig({
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineConfig(async () => ({
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  //
+  // 1. prevent vite from obscuring rust errors
+  clearScreen: false,
+  // 2. tauri expects a fixed port, fail if that port is not available
+  server: {
+    port: 1420,
+    strictPort: true,
+    host: host || false,
+    hmr: host
+      ? {
+          protocol: "ws",
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      // 3. tell vite to ignore watching `src-tauri`
+      ignored: ["**/src-tauri/**"],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -11,7 +34,4 @@ export default defineConfig({
     outDir: "dist",
     target: "esnext",
   },
-  server: {
-    open: true,
-  },
-});
+}));
