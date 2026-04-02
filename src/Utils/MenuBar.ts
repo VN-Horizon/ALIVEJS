a4f5e38e52fec38f2b357dc47a1d1d65349c7cb3
LC_022 <101245833+sblzdddd@users.noreply.github.com>
2026-04-02 10:47:54 +0800

    refactor save-load system and UI

--- src/Utils/MenuBar.ts
+++ src/Utils/MenuBar.ts
@@ -1,3 +1,9 @@
+import { execUntilNextLine } from "../Core/Events";
+import { applyGameState, loadGame, saveGame } from "../Core/GameSave";
+import { destroyScene } from "../Scene/SceneManagement";
+import { loadBackgroundScene } from "../Scripts/BACKGROUND";
+import { pushDialogWindow } from "../Scripts/WINDOW/WINDOW";
+
 export function initMenuBar() {
   const menuItems = document.querySelectorAll(".menu-bar-item");
   if (menuItems.length === 0) return;
@@ -10,6 +16,39 @@ export function initMenuBar() {
       item.querySelector("button")?.blur();
     });
   }
+
+  const saveMenu = document.getElementById("menu-save");
+  const loadMenu = document.getElementById("menu-load");
+
+  saveMenu?.addEventListener("click", async () => {
+    if (saveMenu.getAttribute("aria-disabled") === "true") return;
+    const slotIndex = await window.openSaveLoadDialog("save");
+    if (slotIndex !== null) {
+        saveGame(slotIndex);
+    }
+  });
+
+  loadMenu?.addEventListener("click", async () => {
+    if (loadMenu.getAttribute("aria-disabled") === "true") return;
+    const slotIndex = await window.openSaveLoadDialog("load");
+    if (slotIndex !== null) {
+        const gameState = loadGame(slotIndex);
+        if (gameState) {
+            // First destroy current scene if needed, or push UI
+            applyGameState(gameState);
+            
+            // Check if we are outside of dialogue.
+            if (saveMenu?.getAttribute("aria-disabled") === "true") {
+                await loadBackgroundScene();
+                await pushDialogWindow();
+                saveMenu?.removeAttribute("aria-disabled");
+            } else {
+                destroyScene();
+            }
+            execUntilNextLine();
+        }
+    }
+  });
 
   menuItems.forEach((item) => {
     const button = item.querySelector("button");
